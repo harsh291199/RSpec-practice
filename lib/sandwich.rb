ae3cc9cfad5dcc8866657e4c6ce555ccd4570f73
harsh291199 <harshsuthar2911@gmail.com>
2021-09-03 16:43:38 +0530

    Build and Add own RSpec

--- lib/sandwich.rb
+++ lib/sandwich.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Sandwitch class
+# Sandwich class
 class Sandwich
   attr_accessor :meat, :cheese, :condiments
 
---
+++ spec/dog_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Dog' do
+  context 'when it is hungry' do
+    it 'begs for food' do
+      expect(true).to eq(true)
+    end
+  end
+end
